4c809647d1b3661ee0c301f4a3e568cf2216ffdf
meesalamahalakshmi23 <meesalamahalakshmi23@gmail.com>
2025-05-27 21:58:10 +0530

    Add files via upload

---
+++ frist.py
@@ -0,0 +1,60 @@
+count=0
+for i in range (1,41):
+  if i%5==0 and i%7==0:
+    count+=1
+print(count)
+
+n=5
+fact=1
+for i in range(1,n+1):
+  fact*=i
+print(fact)  
+
+count=0
+for i in range (1,51):
+  if i%7==0:
+    count+=1
+print(count)
+
+for i in range(1,20):
+    if i%2==0:
+         print(i) 
+        
+for i in range(1,11):
+      print(i)
+    
+for i in range(1,6):
+    print(i**2)
+    
+sum=0   
+for i in range(1,11):
+    sum=sum+i
+print(sum) 
+
+
+for i in range(1,15):
+    if i%2!=0:
+        print(i)
+        
+        
+for i in range(1,8):
+    print(i**3)       
+
+    
+    
+    
+count=0
+for i in range(1,51):
+    if i%7==0:
+        count+=1
+print(count)  
+
+
+
+n=int(input())
+for i in range(1,11):
+    print(n*i)
+    
+
+for i in range(10,0,-1):
+     print(i)
